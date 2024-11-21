79ec234a42c00495c5d0d52dae109e9187da37e4
ArioVeisa <classlearning04g@gmail.com>
2024-11-21 23:15:07 +0700

    blade

---
+++ app/Models/Post.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Post
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'judul-post-1',
+                'title' => 'judul post 1',
+                'author' => 'randomname',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit a alias laborum, ex
+amet, recusandae eius
+cupiditate praesentium, minima minus harum doloremque est beatae suscipit odit perferendis ab doloribus quae
+accusamus corporis porro? Molestiae earum reprehenderit eligendi rerum excepturi necessitatibus error, ea
+hic cumque debitis, voluptatem eos culpa rem, assumenda minima? Reprehenderit consequuntur'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-post-2',
+                'title' => 'judul post 2',
+                'author' => 'randomname2',
+                'body' => 'Mnima minus harum doloremque est beatae suscipit odit perferendis ab doloribus quae
+accusamus corporis porro? Molestiae earum reprehenderit eligendi rerum excepturi necessitatibus error, ea
+hic cumque debitis, voluptatem eos culp
+a rem, assumenda minima? Reprehenderit consequuntur'
+            ],
+        ];
+    }
+    public static function find($slug) : array
+    {
+        // $posts = static::all();
+        // return  Arr::first(static::all(), function ($post) use ($slug) {
+        //     return $post['slug'] == $slug;
+        // });
+
+        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
+
+        if (!$post) {
+            abort(404);
+        }
+
+        return $post;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,66 +1,22 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'judul-post-1',
-            'title' => 'judul post 1',
-            'author' => 'randomname',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit a alias laborum, ex
-            amet, recusandae eius
-            cupiditate praesentium, minima minus harum doloremque est beatae suscipit odit perferendis ab doloribus quae
-            accusamus corporis porro? Molestiae earum reprehenderit eligendi rerum excepturi necessitatibus error, ea
-            hic cumque debitis, voluptatem eos culpa rem, assumenda minima? Reprehenderit consequuntur'
-        ],
-        [
-            "id" => 2,
-            "slug" => "judul-post-2",
-            'title' => 'judul post 2',
-            'author' => 'randomname2',
-            'body' => 'Mnima minus harum doloremque est beatae suscipit odit perferendis ab doloribus quae
-            accusamus corporis porro? Molestiae earum reprehenderit eligendi rerum excepturi necessitatibus error, ea
-            hic cumque debitis, voluptatem eos culpa rem, assumenda minima? Reprehenderit consequuntur'
-        ],
-
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            "slug" => "judul-post-1",
-            'title' => 'judul post 1',
-            'author' => 'randomname',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit a alias laborum, ex
-        amet, recusandae eius
-        cupiditate praesentium, minima minus harum doloremque est beatae suscipit odit perferendis ab doloribus quae
-        accusamus corporis porro? Molestiae earum reprehenderit eligendi rerum excepturi necessitatibus error, ea
-        hic cumque debitis, voluptatem eos culpa rem, assumenda minima? Reprehenderit consequuntur'
-        ],
-        [
-            "id" => 2,
-            "slug" => "judul-post-2",
-            'title' => 'judul post 2',
-            'author' => 'randomname2',
-            'body' => 'Mnima minus harum doloremque est beatae suscipit odit perferendis ab doloribus quae
-        accusamus corporis porro? Molestiae earum reprehenderit eligendi rerum excepturi necessitatibus error, ea
-        hic cumque debitis, voluptatem eos culpa rem, assumenda minima? Reprehenderit consequuntur'
-        ],
 
-    ];
-
-
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+    $post = Post::find($slug);
 
     return view('post', ['title' => 'single post', 'post' => $post]);
 });
